eba32ffa5801445045da49832314821060fd53f7
Makiv-Vik <viktorkaktysikov@gmail.com>
2022-02-23 15:05:51 +0200

    change to success tests

--- list.test.js
+++ list.test.js
@@ -16,21 +16,21 @@ const object = {
 
 test('start with empty List', () => {
   const list = new LinkedList();
-  expect(list).toEqual({});
+  expect(list).toEqual(clearLIst);
 });
 
 test('add first item string "a"', () => {
   const list = new LinkedList();
 
   expect(list.append('a')).toBe();
-  expect(list).toEqual({});
+  expect(list).toEqual(object);
 });
 
 test('parse list to array', () => {
   const list = new LinkedList();
 
   expect(list.append('a')).toBe();
-  expect(list.toArray()).toEqual(node);
+  expect(list.toArray()).toEqual([node]);
 });
 
 test('parse values of list to type string', () => {
@@ -39,7 +39,6 @@ test('parse values of list to type string', () => {
 
   expect(list.append('a')).toBe();
   expect(list.append('b')).toBe();
-  expect(list.append('c')).toBe();
   expect(list.append('c')).toBe();
   expect(list.toString(nodeStringifier)).toEqual('abc');
 });
@@ -50,7 +49,7 @@ test('determine the length of List', () => {
   expect(list.append('a')).toBe();
   expect(list.append('b')).toBe();
   expect(list.append('c')).toBe();
-  expect(list.length()).toEqual(6);
+  expect(list.length()).toEqual(3);
 });
 
 test('insert value "Q" at position 2 on list "abcde"', () => {
@@ -62,7 +61,7 @@ test('insert value "Q" at position 2 on list "abcde"', () => {
   expect(list.append('d')).toBe();
   expect(list.append('e')).toBe();
 
-  expect(list.insert('Q', 4)).toBe();
+  expect(list.insert('Q', 2)).toBe();
   expect(`${list}`).toEqual('abQcde');
 });
 
@@ -76,7 +75,7 @@ test('delete value "C" at position 2 on list "abCde"', () => {
   expect(list.append('e')).toBe();
 
   expect(list.delete(2)).toBe('C');
-  expect(`${list}`).toEqual('abCde');
+  expect(`${list}`).toEqual('abde');
 });
 
 test('delete all values "C" on list "CabCdeC"', () => {
@@ -91,7 +90,7 @@ test('delete all values "C" on list "CabCdeC"', () => {
   expect(list.append('C')).toBe();
 
   expect(list.deleteAll('C')).toBe();
-  expect(`${list}`).toEqual('Cabde');
+  expect(`${list}`).toEqual('abde');
 });
 
 test('get value from list by index "2"', () => {
@@ -103,7 +102,7 @@ test('get value from list by index "2"', () => {
   expect(list.append('d')).toBe();
   expect(list.append('e')).toBe();
 
-  expect(list.get(2)).toBe('b');
+  expect(list.get(2)).toBe('c');
 });
 
 test('get value from list by index "2"', () => {
@@ -119,7 +118,7 @@ test('get value from list by index "2"', () => {
   const listClone = list.clone();
 
   expect(list.append('NEW_ITEM')).toBe();
-  expect(`${listClone}`).toEqual('abcdeNEW_ITEM');
+  expect(`${listClone}`).toEqual('abcde');
 });
 
 test('reverse Nodes from list', () => {
@@ -132,7 +131,7 @@ test('reverse Nodes from list', () => {
   expect(list.append('e')).toBe();
 
   expect(list.reverse()).toBe();
-  expect(`${list}`).toEqual('abcde');
+  expect(`${list}`).toEqual('edcba');
 });
 
 test('find position of the first matching element', () => {
@@ -145,7 +144,7 @@ test('find position of the first matching element', () => {
   expect(list.append('C')).toBe();
   expect(list.append('e')).toBe();
 
-  expect(list.findFirst('C')).toBe(4);
+  expect(list.findFirst('C')).toBe(2);
 });
 
 test('find position of the last matching element', () => {
@@ -157,8 +156,6 @@ test('find position of the last matching element', () => {
   expect(list.append('d')).toBe();
   expect(list.append('C')).toBe();
   expect(list.append('e')).toBe();
-
-  expect(list.append('C')).toBe();
 
   expect(list.findLast('C')).toBe(4);
 });
@@ -174,7 +171,7 @@ test('clear - delete all items from list', () => {
   expect(list.append('e')).toBe();
 
   expect(list.clear()).toBe();
-  expect(list).toEqual({});
+  expect(list).toEqual(clearLIst);
 });
 
 test('extend list1 by list 2', () => {
@@ -189,5 +186,5 @@ test('extend list1 by list 2', () => {
   expect(list2.append('e')).toBe();
 
   expect(list.extend(list2)).toBe();
-  expect(`${list}`).toEqual('abc');
+  expect(`${list}`).toEqual('abcde');
 });
